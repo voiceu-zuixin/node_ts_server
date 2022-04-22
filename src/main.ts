2bc77517dc3ee8e681ecea80c113eac580b92a34
voiceu <voiceu@qq.com>
2022-04-22 10:32:00 +0800

    配置husky并测试不标准代码是否自动修正-15

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ console.log(test)
 // import的时候才有http的类型
 import http from 'http'
 const path = require('path')
-const aaas: number = '1'
+const aaas: number
+= '1'
 
 //这样引入可以看到类型，上面那种应该也可以，只不过http可能真的是any类型
 // import axios from "axios";
